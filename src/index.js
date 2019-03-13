6dd734820eccc16bad68126dabf752fe973bce5e
stathamby <my.ipo4ta@gmail.com>
2019-03-13 21:43:11 +0300

    feat: function which calculates how many love triangles phenomenons take place

--- src/index.js
+++ src/index.js
@@ -1,7 +1,23 @@
 /**
  * @param preferences - an array of integers. Indices of people, whom they love
- * @returns number of love triangles
- */
+ * @returns number of love triangles*/
+ 
 module.exports = function getLoveTrianglesCount(preferences = []) {
-  // your implementation
-};
+  
+ /**let preferences=[2,4,3,1,6,7,5];*/
+    let result = 0;
+    const arr = [];
+    for(let i = 0; i < preferences.length; i++) 
+      {
+        const personA = preferences[i];
+        const personB = preferences[personA - 1];
+        const personC = preferences[personB - 1]; 
+        if (personC === (i+1) && !arr.includes(personA) && personA != personC)
+        {
+          result++;
+          arr.push(personA, personB, personC);
+          console.log(arr);
+        }
+      }
+    return result;
+  }
